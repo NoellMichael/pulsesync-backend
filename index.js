0598e4c8f2ab96fdaa09c09bd759a4f3d65f814c
Noell Biju Michael <noellbijumichael@gmail.com>
2024-02-18 19:25:39 +0530

    hi

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
 import { initializeApp, applicationDefault } from 'firebase-admin/app';
-import { getMessaging, onMessage } from 'firebase-admin/messaging';
-import express from 'express';
-import cors from 'cors';
+import { getMessaging, onBackgroundMessage } from "firebase/messaging";
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
 app.use(
   cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   })
 );
 
 app.use(function (req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json");
   next();
 });
 
 initializeApp({
   credential: applicationDefault(),
-  databaseURL: 'https://pulserythm-1e02c-default-rtdb.asia-southeast1.firebasedatabase.app',
+  databaseURL: "https://pulserythm-1e02c-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
-app.post('/send', function (req, res) {
+const messaging = getMessaging();
+
+onBackgroundMessage((payload) => {
+  console.log('Received background message:', payload);
+  // Handle the background message here
+});
+
+app.post("/send", function (req, res) {
   const receivedToken = req.body.fcmToken;
   const message = {
     notification: {
-      title: 'Notif',
-      body: 'This is a test notification',
+      title: "Notif",
+      body: "This is a test notification"
     },
+    token: receivedToken,  // You can also use `topic` or `condition` instead of `token`
   };
 
   getMessaging()
-    .sendToDevice(receivedToken, message)
+    .send(message)
     .then((response) => {
       res.status(200).json({
-        message: 'msg sent successfully',
+        message: "msg sent successfully",
         token: receivedToken,
       });
-      console.log('msg sent successfully:', response);
+      console.log("msg sent successfully:", response);
     })
     .catch((error) => {
       res.status(400);
       res.send(error);
-      console.log('error sending message:', error);
+      console.log("error sending message:", error);
     });
 });
 
 app.listen(3000, function () {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
